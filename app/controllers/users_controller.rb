47ecf9b5f92b3a42e78a3d47d4d988d7bc926fd4
Anna Karingal <anna.karingal@gmail.com>
2015-08-07 18:47:32 -0400

    Implement update user route, remove password and add profile attrs to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,13 @@ class UsersController < ApplicationController
 
 	def create
 		user = User.new(user_params)
-		if @user.save
+		if user.save
 			session[:user_id] = user.id
 			redirect_to :root
 		else
 			flash.now[:error] = user.errors.full_messages
 		  render :new
 		end
-
 	end
 
   def show
@@ -23,24 +22,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # TODO: Make sure user == current user
     @user = User.find_by(id: params[:id])
   end
 
   def update
-    # TODO: Make sure user == current user
     user = User.find_by(id: params[:id])
-    user.update(user_params)
+    user.update_attributes(user_params)
     if user.save 
       flash[:success] = "Your profile has been updated!"
+      redirect_to user_path(user)
     else
       flash[:error] = user.errors.full_messages
+      redirect_to edit_user_path(user)
     end
-    redirect_to user
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :first_name, :last_name, :email, :username, :password, :password_cofirmation, :zip_code)
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :username, :zip_code, 
+                                 :bio, :github_link, :website_link)
   end
 end
