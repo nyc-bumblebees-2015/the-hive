8d244b34e10fccdbd24ad9eb6c1b5ed2f3a1d925
Jason Guzik <jguzik@gmail.com>
2015-08-07 15:38:23 -0400

    add validations to models

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,5 +1,9 @@
 class Collaboration < ActiveRecord::Base
   belongs_to :collaborator, class_name: "User"
   belongs_to :project
+
+  validates :collaborator_id, presence: true
+  validates :project_id, presence: true
+  validates :status, presence: true
 end
 
--- app/models/project_tag.rb
+++ app/models/project_tag.rb
@@ -1,4 +1,7 @@
 class ProjectTag < ActiveRecord::Base
   belongs_to :project
   belongs_to :tag
+
+  validates :project_id, presence: true
+  validates :tag_id, presence: true
 end
