ab0c229ffd4d33e33b4ce2deeb7239c0c0971f72
AlinaJahnes <alina.jahnes@gamil.com>
2015-08-07 16:27:10 -0400

    fix form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,18 @@ class UsersController < ApplicationController
 
 	def create
 		user = User.new(user_params)
-		if @user.save
+		if user.save
 			session[:user_id] = user.id
 			redirect_to :root
 		else
 			flash.now[:error] = user.errors.full_messages
-			render :new
-		end	
+		  render :new	
+		end
+				
 	end
 
-private
-  def user_params
-		params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation,:zip_code)
-	end
+	private
+	  def user_params
+			params.require(:user).permit(:username, :first_name, :last_name, :email, :username, :password, :password_cofirmation, :zip_code)
+		end
 end
