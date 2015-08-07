6906b654e5c3ba60947616927b70e745d23decc8
Jason Guzik <jguzik@gmail.com>
2015-08-07 12:19:04 -0400

    create geocode location based on zip code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  geocoded_by :zip_code
+  before_validation :geocode
 
   has_many :projects_created, class_name: "Project", foreign_key: "creator_id"
   has_many :collaborations, foreign_key: "collaborator_id"
