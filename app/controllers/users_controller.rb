960f783a1995414915c7f292127ac9088da3fa83
Anna Karingal <anna.karingal@gmail.com>
2015-08-07 16:39:58 -0400

    Implement show, edit & update routes for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,44 @@
 class UsersController < ApplicationController
+
 	def new
 		@user = User.new
 	end
 
 	def create
-		@user = User.new(user_params)
+		user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
+			session[:user_id] = user.id
 			redirect_to :root
 		else
-			flash.now[:error] = @user.errors.full_messages
+			flash.now[:error] = user.errors.full_messages
 		  render :new
 		end
 
 	end
 
-	private
-	  def user_params
-			params.require(:user).permit(:username, :first_name, :last_name, :email, :username, :password, :password_cofirmation, :zip_code)
-		end
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    # TODO: Make sure user == current user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    # TODO: Make sure user == current user
+    user = User.find_by(id: params[:id])
+    user.update(user_params)
+    if user.save 
+      flash[:success] = "Your profile has been updated!"
+    else
+      flash[:error] = user.errors.full_messages
+    end
+    redirect_to user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :username, :password, :password_cofirmation, :zip_code)
+  end
 end
