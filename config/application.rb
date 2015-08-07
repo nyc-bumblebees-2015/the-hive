790071d6836f1b4d5a2a21561824ef3c1ed34110
Jason Guzik <jguzik@gmail.com>
2015-08-07 17:14:39 -0400

    finish rspec configuraion

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,14 @@ module TheHive
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+config.generators do |g|
+    g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+    g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
