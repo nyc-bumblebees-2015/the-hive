2dff67b28b486e4af693ee150b16c4ee973fd278
Anna Karingal <anna.karingal@gmail.com>
2015-08-07 18:50:23 -0400

    Set password for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
 	def create
 		user = User.new(user_params)
+    user.password = params[:user][:password]
 		if user.save
 			session[:user_id] = user.id
 			redirect_to :root
