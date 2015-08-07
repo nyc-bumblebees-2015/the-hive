164489a4c30f9a913c87e0af1a3b1396d6b4056d
Anna Karingal <anna.karingal@gmail.com>
2015-08-07 15:16:32 -0400

    Seed with users, projects, tags & collabs

--- db/seeds.rb
+++ db/seeds.rb
@@ -122,9 +122,52 @@ TECH_TAGS = [
   "stella"
 ]
 
+COLLAB_STATUSES = ["pending", "approved", "denied"]
+
+# Create tags
 TECH_TAGS.each do |t|
   Tag.create(name: t)
 end
 
+# Create Users
+10.times do 
+  name = Faker::Name.name.split(" ")
+  first_name = name[0]
+  last_name = name[1]
+  User.create(username: Faker::Internet.user_name, 
+              email: Faker::Internet.email, 
+              password: "password", 
+              zip_code: Faker::Address.zip.to_s, 
+              first_name: first_name,
+              last_name: last_name,
+              bio: Faker::Lorem.paragraph,
+              website_link: Faker::Internet.url,
+              github_link: Faker::Internet.url('github.com')
+              )
+end
 
+# Create
+15.times do 
+  user_id = rand(1..User.count)
+  Project.create(creator_id: user_id, 
+                 title: Faker::App.name, 
+                 description: Faker::Lorem.paragraph, 
+                 skills_desired: Faker::Lorem.paragraph, 
+                 repo_link: Faker::Internet.url('github.com'),
+                 start_date: Faker::Date.between(365.days.ago, Date.today),
+                 end_date: Faker::Date.forward(365)
+                 )
+end
 
+User.all.each do |user|
+  rand(1..3).times do 
+    Collaboration.new(collaborator_id: user.id, 
+                    project_id: rand(1..Project.count), 
+                    status: COLLAB_STATUSES.sample
+                    )
+  end
+end
+
+Projects.all.each do |project|
+  project.tags << TECH_TAGS.sample(rand(1..5))
+end
