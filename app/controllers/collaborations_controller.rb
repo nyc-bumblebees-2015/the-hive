982240a85126f26a331b2fdc8f2d95f6db58870e
Anna Karingal <anna.karingal@gmail.com>
2015-08-08 15:11:15 -0400

    Create route adds new collab to project

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,2 +1,14 @@
 class CollaborationsController < ApplicationController
+  def create
+    require_login
+    project = Project.find_by(id: params[:project_id])
+    if project 
+      project.collaborations << Collaboration.new(collaborator_id: current_user.id)
+      flash[:success] = "You have requested to collaborate on #{project.title}"
+      redirect_to project_path(project)
+    end
+  end
+
+  def update
+  end
 end
