22ba17e201d0a9c3f1d2b8b3c280e2fba895eab5
Anna Karingal <anna.karingal@gmail.com>
2015-08-08 15:24:34 -0400

    Standardize flash error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to :root
     else
-      flash.now[:error] = ["Incorrect username or password."]
+      flash.now[:errors] = ["Incorrect username or password."]
       render :new
     end
   end
