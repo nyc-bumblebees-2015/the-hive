70b737514251c6c4aaf014ac9a0bacfc9316017d
Christine Schatz <cmschatz@gmail.com>
2015-08-10 14:49:38 -0400

    Implement ajax for replacing div content with search results

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-  $('')
+  $('#project_search_form').on('submit', search);
+});
 
-
-
-
-})
+var search = function(event) {
+  event.preventDefault();
+  var $target = $(event.target);
+  $.ajax({
+    url: $target.attr('action'),
+    method: 'GET',
+    data: $target.serialize(),
+  }).done(function(response){
+    console.log("yay", response);
+    $('.near-you').replaceWith(response);
+  }).fail(function(error){
+    console.log("whoops, no good.")
+  })
+};
