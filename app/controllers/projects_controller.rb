ed6bee1ed73b0ab75142722331d5f6ae75786307
Christine Schatz <cmschatz@gmail.com>
2015-08-10 12:32:32 -0400

    Remove pagination

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -74,7 +74,7 @@ class ProjectsController < ApplicationController
       @users = User.near(params[:search], params[:distance].to_i, :order => 'distance').where.not(:id => current_user.id)
     end
     @projects = []
-    @users.each do |user| 
+    @users.each do |user|
       user.projects_created.each do |project|
         @projects << project
       end
